aa144cb7667a73c6e109e98c380a4ef2ad146bdc
ryan-rashidian <ryanrashidian@gmail.com>
2026-05-30 00:49:56 -0700

    Upgrade log.h logging utility to one im currently using

--- log/log.h
+++ log/log.h
@@ -1,28 +1,60 @@
 /* 
- * log - Logging macro
+ * log.h - Logging utility
  */
 
 #ifndef LOG_H
 #define LOG_H
 
-#if LOG_ENABLED
+#define LOG(l , ...) logger((l), __VA_ARGS__)
 
-#include <stdio.h>
-#include <stddef.h>
+enum {
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR,
+};
 
-#define LOG(f, m) \
-do { \
-    FILE *fp = fopen(f, "ab"); \
-    if (fp != NULL) { \
-        fprintf(fp, "%s: line %d: LOG msg: %s\n", \
-                    __FILE__, __LINE__, m); \
-    } \
-    fclose(fp); \
-} while(0)
+void logger(int level, const char *message, ...);
 
-#else
-#define LOG(f, m) // empty macro if disabled
 #endif
 
+#ifdef SRZ_LOG_IMPLEMENTATION
+#ifndef SRZ_LOG_IMPL_GUARD
+#define SRZ_LOG_IMPL_GUARD
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define LOG_BUFFER_MAX 256
+
+static int log_level = LOG_INFO;
+
+static char *log_level_strings[] = {
+    "[LOG]: DEBUG: ",
+    "[LOG]: INFO: ",
+    "[LOG]: WARNING: ",
+    "[LOG]: ERROR: ",
+};
+
+void logger(int level, const char *message, ...)
+{
+    if (level < log_level || message == NULL) return;
+
+    char buffer[LOG_BUFFER_MAX] = { 0 };
+    size_t lvl_len = strlen(log_level_strings[level]);
+    size_t msg_len_max = LOG_BUFFER_MAX - lvl_len - 2;
+
+    memcpy(buffer, log_level_strings[level], lvl_len);
+    strncat(buffer, message, msg_len_max);
+    strcat(buffer, "\n");
+    
+    va_list va;
+    va_start(va, message);
+    vfprintf(stderr, buffer, va);
+    va_end(va);
+}
+
+#endif
 #endif
 
